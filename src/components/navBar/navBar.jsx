020e50a67d62289810c7c15a439948a4b0851bf7
TefaSalcedo <stefa.dtbu@gmail.com>
2024-03-21 10:21:09 -0500

    Finalización css responsive

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navBar.css";
 import Boton from "../boton/boton.jsx";
+import open from "./open.svg";
+import close from "./close.svg";
 
 const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    console.log(isMenuOpen); // Agrega esta línea
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
@@ -16,7 +25,30 @@ const NavBar = () => {
           <li className="navbar-item">Products</li>
         </ul>
       </div>
-      <Boton title="Connect Wallet" size="230px" />
+      <div className="navbar-boton">
+        <Boton title="Connect Wallet" size="230px" />
+      </div>
+      <div className="navbar-menu">
+        <img
+          src={open}
+          alt="menu-open"
+          className={`navbar-menu-open ${isMenuOpen ? "hidden" : ""}`}
+          onClick={toggleMenu}
+        />
+        <img
+          src={close}
+          alt="menu-close"
+          className={`navbar-menu-close ${isMenuOpen ? "" : "hidden"}`}
+          onClick={toggleMenu}
+        />
+      </div>
+      <div className={`navbar-mobile-menu ${isMenuOpen ? "" : "hidden"}`}>
+        <ul className="navbar-mobile-hambuger-menu">
+          <li>Markets</li>
+          <li>Portafolio</li>
+          <li>Products</li>
+        </ul>
+      </div>
     </nav>
   );
 };
--- src/components/transactions/transactions.jsx
+++ src/components/transactions/transactions.jsx
@@ -1,12 +1,33 @@
+import React, { useState, useEffect } from 'react';
 import DataTransaction from "./dataTransation";
 import IndividualTS from "./individualTS.jsx";
 import "./transactions.css";
 
-const Transaction = () => (
+const Transaction = () => {
+    // Estado inicial
+    const [isScreenSmall, setIsScreenSmall] = useState(window.innerWidth <= 768);
+
+    useEffect(() => {
+      // Función para actualizar el estado
+      const checkScreenSize = () => {
+        setIsScreenSmall(window.innerWidth <= 768);
+      };
+  
+      // Agrega el listener
+      window.addEventListener('resize', checkScreenSize);
+  
+      // Elimina el listener cuando el componente se desmonta
+      return () => window.removeEventListener('resize', checkScreenSize);
+    }, []);
+  
+    // Filtra los datos si la pantalla es pequeña
+    const filteredData = isScreenSmall ? DataTransaction.slice(0, 2) : DataTransaction;
+  
+  return(
   <div className="content-transactions">
     <span className="transaction-title">Latest Transactions</span>
     <div className="Grid-gifs">
-      {DataTransaction.map((DataETH, position) => (
+      {filteredData.map((DataETH, position) => (
         <IndividualTS
           key={position}
           ffrom={DataETH.from}
@@ -19,6 +40,7 @@ const Transaction = () => (
       ))}
     </div>
   </div>
-);
+  );
+}
 
 export default Transaction;
